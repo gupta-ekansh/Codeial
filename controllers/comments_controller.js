4c2b2924d87e9cc80ebbf4423477c4eb2b904405
Ekansh Gupta <ekanshgupta2019@gmail.com>
2022-11-19 22:30:00 +0530

    Added Codeial

---
+++ controllers/comments_controller.js
@@ -0,0 +1,52 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+
+module.exports.create = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.body.post);
+
+        if (post){
+            let comment = await Comment.create({
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id
+            });
+
+            post.comments.push(comment);
+            post.save();
+            req.flash('success' , 'Comment added successfully');
+            res.redirect('/');
+        }
+    }catch(err){
+        req.flash('error' , 'Could not add this comment');
+        return res.redirect('back');
+    }
+    
+}
+
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let comment = await Comment.findById(req.params.id);
+
+        if (comment.user == req.user.id){
+
+            let postId = comment.post;
+
+            comment.remove();
+
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+            req.flash('success' , 'Comment deleted successfully');
+            return res.redirect('back');
+        }else{
+            req.flash('success' , 'You are not authorised to delete this comment');
+            return res.redirect('back');
+        }
+    }catch(err){
+        req.flash('error' , 'Could not add this comment');
+        return res.redirect('back');
+    }
+    
+}
